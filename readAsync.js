f12e551f7c74879b80d00ede440eb782cf331dde
Jose Guzman <joseguzmanzamora@gmail.com>
2018-09-13 08:22:27 -0600

    source code

--- readAsync.js
+++ readAsync.js
@@ -1,11 +1,14 @@
 var fs = require('fs')
+var contador = 0
 //Read file function
 function readFile(){
     var buffer = fs.readFile("./fileTest") 
     var textPlain = buffer.toString()
     var myArray = textPlain.split("\n")
     myArray.forEach(function(value){
+        contador += 1 
         console.log(value);
+        console.log(contador);
     });
 }
 readFile()
--- readAsyncCb.js
+++ readAsyncCb.js
@@ -2,12 +2,29 @@ var fs = require('fs')
 //Read file function
 function callback  (err, data) {
     if (err) return console.error(err);
-    var textPlain = data.toString()
-    console.log(textPlain)
+    var textPlain = data.toString();
+    lines(textPlain);
 };
+
 function readFile(){
     //fs.readFile("./fileTest", function (err, data) {
-    var buffer = fs.readFile("./fileTest",callback )
-  
+    var buffer = fs.readFile("./fileTest",callback );
 }
+
+function lines(text){
+    var contador = 1 
+    var espacio = '\n'
+    var lineas = text.split('\n')
+    for(var j = 0; j < lineas.length; j++){
+        for(var i = 0; i < lineas[j].length; i++){
+            if(lineas[j][i] == " ")
+            contador += 1;
+        }
+        console.log(lineas[j]);
+        console.log(contador);
+        fs.appendFileSync("./fileTest", (espacio.concat(contador.toString())));
+        contador = 1 ;
+    }
+}
+
 readFile()
